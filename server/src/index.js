491c893d98a7cb153077d609b075f8ea5113cf81
marco-amorim <marcoamorim95@hotmail.com>
2020-07-30 17:55:30 -0300

    added dotenv dependency

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(morgan('common'));
