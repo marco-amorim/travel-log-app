42ba028c08f12f3c3a084fd53040e6b2b9dc021a
marco-amorim <marcoamorim95@hotmail.com>
2020-07-31 18:30:04 -0300

    middlewares functions in a separated file

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const middlewares = require('./middlewares');
 require('dotenv').config();
 
 const app = express();
@@ -19,24 +20,12 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
+app.use(middlewares.notFound);
 
-// eslint-disable-next-line
-app.use((error, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🕵️‍♂️' : error.stack,
-  });
-});
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port} 🚀`);
 });
