179c7746e0384b110261e2da0a08e2d3b43fe7d8
marco-amorim <marcoamorim95@hotmail.com>
2020-08-02 23:39:01 -0300

    added api key for security and built the frontend

--- client/src/api.js
+++
@@ -1,6 +0,0 @@
-const API_URL = 'http://localhost:2020';
-
-export async function listLogEntries() {
-	const response = await fetch(`${API_URL}/api/logs`);
-	return response.json();
-}
--- server/src/middlewares.js
+++ server/src/middlewares.js
@@ -4,13 +4,13 @@ const notFound = (req, res, next) => {
   next(error);
 };
 
-// eslint-disable-next-line
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (error, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({
     message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? '🕵️‍♂️' : error.stack,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
   });
 };
 
